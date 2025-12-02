eecaa958c1c928f1df96b4b84c391c77fb6a092e
Thorium234 <inyangalapaul5@gmail.com>
2025-12-02 17:13:33 +0300

    feat: Integrate Bootstrap CSS for professional styling

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+// tailwind.config.js
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './video/templates/**/*.html',
+    './thoriumcall/templates/**/*.html', // If you have project-level templates
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
